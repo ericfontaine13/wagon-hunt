fd6cbe6e9030dcacf2e2c91be93ad25ccc687612
Eric Fontaine <ericfontaine13@gmail.com>
2016-08-22 17:05:33 +0200

    add resources routing and CRUD actions for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,24 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id].to_i)
   end
 
+  def new
+    @product = Product.create(:name,
+  end
+
+  def create
+    @product = Product.create(:name,
+  end
+
+  def edit
+    @product = Product.create(:name,
+  end
+
+  def update
+    @product = Product.create(:name,
+  end
+
+  def destroy
+    @product = Product.create(:name,
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,27 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get "/team" => "pages#team"
-  get "/products" => "products#index"
-  get "/products/:id" => "products#show"
+  get "/contact" => "pages#contact"
+
+  resources :products
+
+  # #Read action routes
+  # get "/products" => "products#index"
+  # get "/products/:id" => "products#show"
+
+
+  # #Create action routes
+  # get "/products/new" => "products#new"
+  # post "/products" => "products#create"
+
+
+  # #Update action routes
+  # get "/products/:id/edit" => "products#edit"
+  # patch "/products/:id" => "products#update"
+
+  # #Delete action routes
+  # delete "/products/:id" => "products#destroy"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
